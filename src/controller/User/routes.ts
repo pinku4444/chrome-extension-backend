a1168afc6e6f1cc29e48c2e4e4ccf58f4339daed
Pankaj <pankaj.sharma@successive.tech>
2019-11-06 14:20:11 +0530

    fixed conflict

--- src/controller/User/routes.ts
+++ src/controller/User/routes.ts
@@ -8,13 +8,9 @@ UserRoutes.route("/signup").post(controller.signup);
 UserRoutes.route("/login").post(controller.login);
 UserRoutes.route("/getunverified").get(authmeddleware('getUsers', 'all'), controller.getZeros);
 UserRoutes.route("/verified").put(controller.verified);
-<<<<<<< HEAD
 UserRoutes.route("/delete").delete(authmeddleware('getUsers', 'all'), controller.delete);
 UserRoutes.route("/updateFunction").post(authmeddleware('getUsers', 'all'), controller.update);
 UserRoutes.route("/checkFunction").get(controller.getFunction);
-=======
-UserRoutes.route("/delete").post(authmeddleware('getUsers', 'all'), controller.delete);
-UserRoutes.route("/updateFunction").post(authmeddleware('getUsers', 'all'), controller.update);
->>>>>>> 7c83cae4e430ed616bcb311eced8b38a8f7cdba0
+
 
 export { UserRoutes };
